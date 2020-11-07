f2010edb82bb7f2440f847cd46efb65747e3ec72
eutialia <dev@ebon.network>
2020-11-06 16:39:31 -0800

    Visual adjustment

--- src/components/snippetComponent.tsx
+++ src/components/snippetComponent.tsx
@@ -4,7 +4,7 @@ import colabLogo from "../images/colab.png"
 import jupyterLogo from "../images/jupyter.png"
 import kaggleLogo from "../images/kaggle.png"
 import vscodeLogo from "../images/vscode.png"
-import licenseIcon from "../images/license.png"
+import licenseIcon from "../images/license.svg"
 
 export const CodeSnippet: React.FC = () => {
   return (
@@ -25,6 +25,8 @@ export const IntegrationSnippet: React.FC = () => {
     <div
       style={{
         width: "350px",
+        position: "relative",
+        top: "90px",
       }}
     >
       <img src={graph} alt="Integration" width="350" />
@@ -37,23 +39,25 @@ export const LogoSnippet: React.FC = () => {
     <div
       style={{
         display: "flex",
-        flexFlow: "row wrap",
-        width: "310px",
-        justifyContent: "space-evenly",
+        flexFlow: "column wrap",
+        height: "250px",
+        justifyContent: "flex-start",
         alignItems: "center",
+        position: "relative",
+        top: "60px",
       }}
     >
-      <div>
+      <div style={{ flex: "0 1 150px" }}>
         <img src={jupyterLogo} alt="Jupyter logo" width="100" />
       </div>
-      <div>
-        <img src={colabLogo} alt="Colab logo" width="150" />
+      <div style={{ flex: "0 1 80px" }}>
+        <img src={kaggleLogo} alt="Kaggle" width="120" />
       </div>
-      <div>
-        <img src={kaggleLogo} alt="Kaggle" width="150" />
-      </div>
-      <div>
+      <div style={{ flex: "0 1 150px" }}>
         <img src={vscodeLogo} alt="VScode logo" width="100" />
+      </div>
+      <div style={{ flex: "0 1 150px" }}>
+        <img src={colabLogo} alt="Colab logo" width="150" />
       </div>
     </div>
   )
@@ -64,17 +68,19 @@ export const OpensourceSnippet: React.FC = () => {
     <div
       style={{
         display: "flex",
-        flexFlow: "row nowrap",
-        width: "310px",
-        justifyContent: "space-evenly",
+        flexFlow: "column nowrap",
+        height: "150px",
+        justifyContent: "space-between",
         alignItems: "center",
+        position: "relative",
+        top: "100px",
       }}
     >
       <iframe
         src="https://ghbtns.com/github-btn.html?user=sfu-db&repo=dataprep&type=star&count=true&size=large"
         frameBorder="0"
         scrolling="0"
-        width="170"
+        width="135"
         height="30"
         title="GitHub"
       ></iframe>
