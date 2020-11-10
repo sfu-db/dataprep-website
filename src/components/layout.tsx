dbeace46d0ee047b07c348c193e0c5f6b1fe5e76
eutialia <dev@ebon.network>
2020-11-09 22:59:00 -0800

    Initial build

--- src/components/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "../styles/layout.sass"
-
-const Layout: React.FC<React.ReactNode> = ({ children }) => {
-  return <div className="layout-container">{children}</div>
-}
-
-export default Layout
