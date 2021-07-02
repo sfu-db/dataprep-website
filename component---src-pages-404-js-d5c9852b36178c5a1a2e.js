4b85298ed134b035b8c00ca3cc56c33372bf51ee
eutialia <dev@ebon.network>
2021-07-01 18:33:11 -0700

    Updates

--- component---src-pages-404-js-d5c9852b36178c5a1a2e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),u=(n("z6iW"),function(e){var t=e.children;return o.a.createElement("div",{className:"layout-container"},t)});t.default=function(){return o.a.createElement(u,{className:"layout-container"},o.a.createElement("h1",null,"404: Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the VOID."))}},z6iW:function(e,t,n){}}]);
-//# sourceMappingURL=component---src-pages-404-js-d5c9852b36178c5a1a2e.js.map
