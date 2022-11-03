ca55d1f12ab5f5186aa9e2b4210dd5e64cc13bdb
Weiyuan Wu <youngw@sfu.ca>
2022-11-03 03:28:17 +0000

    format slogan

--- components/indexComponents/cards/slogan.tsx
+++ components/indexComponents/cards/slogan.tsx
@@ -2,11 +2,14 @@ const Slogan = () => {
   return (
     <>
       <h1 className="text-primary-500 font-bold leading-tight">
-        Low code data preparation in Python
+        Low-Code 
+      </h1> 
+      <h1 className="text-primary-500 font-bold leading-tight">
+        Data Preparation
       </h1> 
       <p>&nbsp;</p>
       <h4 className="text-primary-200 leading-tight">
-        Collect, clean and visualization your data in python with a few lines of code
+        Collect, clean, and visualize your data in python with a few lines of code
       </h4>
     </>
   )
